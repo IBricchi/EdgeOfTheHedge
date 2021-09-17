b38c50680dca17512c698b094aaaf329afccedb6
Vincent-Sietses <vincent.sietses@gmail.com>
2021-09-17 20:32:39 +0200

    ant kinda fixed
    The ant never seems to get stuck anymore due to raycasting collision detection ... but it now twitches once it gets near an object which it isnt heading towards, not horrible but definitely improvements to be made

--- scripts/ant.gd
+++ scripts/ant.gd
@@ -5,15 +5,18 @@ onready var idle_sprite : AnimatedSprite = $"Basic Ant Idle"
 onready var walk_sprite : AnimatedSprite = $"Basic Ant Walk"
 onready var sensor_area : Area2D = $Area2D
 onready var sensor : CollisionPolygon2D = $"Area2D/AntSensor"
+onready var ray : RayCast2D = $Collisionray
 
-var direction : Vector2
+
+var desired_direction : Vector2 = Vector2.ZERO
 var home : Node setget set_ant_home 
 var speed : float = 1
 
 
+
 enum priority {
 	find_food = 0, 
-	wander = 1,
+	idle = 1,
 	fight = 2,
 	run_away = 3
 }
@@ -28,22 +31,26 @@ var focus : Node = null # focus is the node the ant wants to get to, food, enemy
 
 func _ready():
 	randomize()
-	direction = Vector2(rand_range(-1,-1),rand_range(-1,1)).normalized()
+	desired_direction = Vector2(rand_range(-1,-1),rand_range(-1,1)).normalized()
 	idle_sprite.visible = true
 	walk_sprite.visible = false
 	
 
 func _physics_process(delta):
 	# if the ant is moving
-	if direction.length() != 0 :
+	if ant_priority != priority.idle :
 		if focus :
 			var to_focus : Vector2 = focus.position - self.position
-			direction = (direction + 0.1 * speed* delta*to_focus).normalized()
-			
-		var move_collision_result = move_and_collide( direction * speed)
+			desired_direction = (desired_direction + 0.1 * speed* delta*to_focus).normalized()
+		
+		var raycollision = check_collision_ray(delta)
+		if raycollision:
+			desired_direction =  (desired_direction - 0.25* raycollision.normalized()).normalized()
+		
+		var move_collision_result = move_and_collide( desired_direction * speed)
 		# if the ant collides stop moving
 		if move_collision_result:
-			direction = Vector2(0,0)
+			desired_direction = Vector2(0,0)
 			if move_collision_result.collider == focus:
 				focus_reached()
 			return
@@ -51,9 +58,9 @@ func _physics_process(delta):
 		if not walk_sprite.visible:
 			walk_sprite.visible = true
 			idle_sprite.visible = false
-		walk_sprite.frames.set_animation_speed("Walk", 24* direction.length())
+		walk_sprite.frames.set_animation_speed("Walk", 24* desired_direction.length())
 		# rotate the ant
-		look_at(position + direction)
+		look_at(position + desired_direction)
 		
 		if focus == home:
 			if (home.position - position).length() < 5:
@@ -75,8 +82,17 @@ func _physics_process(delta):
 			walk_sprite.visible = false
 			idle_sprite.visible = true
 			
+func compute_movement():
+	# move in desired direction, avoiding objects
+	return desired_direction * speed	
 		
 		
+func check_collision_ray(delta : float):
+	# rotate the collision ray and check if it hits something, if it does return vector to it 
+	ray.rotation += PI * delta * 2 # rotate once a second	
+	if ray.is_colliding():
+		if (ray.get_collision_point() - focus.position).length() > 25:
+			return ray.get_collision_point() - self.position 
 		
 func focus_reached():
 	if focus.is_in_group("Food"):
@@ -85,7 +101,7 @@ func focus_reached():
 	
 	#rotation -= PI
 	var to_focus : Vector2 = focus.position - self.position
-	direction = to_focus.normalized()
+	desired_direction = to_focus.normalized()
 
 func set_ant_home(vect):
 	home = Node2D.new()
@@ -94,6 +110,6 @@ func set_ant_home(vect):
 	
 func reached_home():
 	focus = null
-	# bounce back in the same direction that the ant came from plusminus pi/2
-	var rand_angle = atan(direction.y / direction.x) +  rand_range(PI/2, 3/2*PI)
-	direction = Vector2(cos(rand_angle), sin(rand_angle))
+	# bounce back in the same desired_direction that the ant came from plusminus pi/2
+	var rand_angle = atan(desired_direction.y / desired_direction.x) +  rand_range(PI/2, 3/2*PI)
+	desired_direction = Vector2(cos(rand_angle), sin(rand_angle))
