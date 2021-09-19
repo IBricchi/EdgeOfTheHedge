4ed7a6bea6113c950141b14141e8a219939bb78e
Vincent-Sietses <vincent.sietses@gmail.com>
2021-09-19 19:32:07 +0200

    fight

--- scripts/ant.gd
+++ scripts/ant.gd
@@ -10,20 +10,23 @@ var marker : Resource = preload("res://scenes/marker.tscn")
 
 var context;
 
-var marker_max_time : float = 2
+var marker_max_time : float = 1
 var markers_set : Array = []
 var marker_follow_timer : float  = 0
 var marker_set_timer : float = 0.4
 var idle_timer :float = 1
 var desired_direction : Vector2 = Vector2(rand_range(-1,-1),rand_range(-1,1)).normalized()
 var home : Node setget set_ant_home 
-var speed : float = rand_range(50,70)
+var speed : float = rand_range(80,100)
 var alive : bool = true
+var damage_per_second : float = 30
+
+
 
 var health_max : float = 20
 var health : float = health_max
 var food_carried :int = 0
-var hunger_max : float = rand_range(40.0,50.0)
+var hunger_max : float = rand_range(70.0,80.0)
 var hunger : float = hunger_max
 
 enum priority {
@@ -54,13 +57,13 @@ func _physics_process(delta):
 		else:
 			marker_follow_timer -= delta
 		
-		check_sensors()
+		check_sensor()
+		
 			
-		manage_movement_and_collision( move_and_collide(delta* desired_direction * speed))
+		manage_movement_and_collision( move_and_collide(delta* desired_direction * speed), delta)
 		
 		# make sure the walk animation is playing and make sure the animation is playing at the correct speed
 		if not walk_sprite.visible:
-			print("walk sprite not visible")
 			walk_sprite.visible = true
 			idle_sprite.visible = false
 		walk_sprite.frames.set_animation_speed("Walk", 24* delta*speed)
@@ -71,7 +74,10 @@ func _physics_process(delta):
 	else:
 		idle_timer -= delta
 		if idle_timer < 0:
-			ant_priority = priority.go_home
+			if food_carried:
+				ant_priority = priority.go_home
+			else:
+				ant_priority = priority.find_food
 			idle_timer = 1
 			desired_direction = - desired_direction
 		if not idle_sprite.visible:
@@ -85,6 +91,7 @@ func _physics_process(delta):
 			food_carried -= 1
 			hunger = hunger_max
 			health = max(health_max, health/2)
+			ant_priority = priority.idle
 	
 	
 	if hunger < hunger_max / 2 or health < health_max/10:
@@ -101,7 +108,7 @@ func set_context(context):
 func update_context(context):
 	modulate = context.color
 
-func check_sensors():
+func check_sensor():
 	if ant_priority == priority.find_food:
 		var min_dist : Vector2 = Vector2(1000,1000)
 		for body in sensor_area.get_overlapping_bodies():
@@ -112,13 +119,43 @@ func check_sensors():
 						min_dist = dist
 		if min_dist.length()< 1000:
 			desired_direction = min_dist.normalized()
-		
+		else:
+			for body in sensor_area.get_overlapping_bodies():
+				if body.is_in_group("marker"):
+					if body.marktype == 1:
+						desired_direction = (body.position-position).normalized()
 	elif ant_priority == priority.go_home:
 		for body in sensor_area.get_overlapping_bodies():
 			if body.is_in_group("queen"):
 				desired_direction = (body.position - position).normalized()
+	elif ant_priority == priority.fight:
+		for body in sensor_area.get_overlapping_bodies():
+			if body.is_in_group("ant"):
+				print("ant spotted")
+				if body.home != home : 
+					desired_direction = (body.position + 3*  body.desired_position - position).normalized()
+	
+func check_fightarea(delta):
+	for area in $fightarea.get_overlapping_areas():
+		if area.get_parent().is_in_group("ant"):
+			print("ant in fight area")
+			if area.get_parent().home != home : 
+				fight(area.get_parent(), delta)
+				return
 	
 	
+	
+func fight(enemyant : Node, delta):
+	if enemyant.alive:
+		print("fighting")
+		look_at(enemyant.position)
+		enemyant.health -= delta * damage_per_second
+		if walk_sprite.visible:
+			walk_sprite.visible = false
+			idle_sprite.visible = true
+		desired_direction = Vector2.ZERO
+	else:
+		desired_direction += position - enemyant.position
 	
 func ant_death():
 	self.modulate = Color(0.1,0.1,0.1)
@@ -154,7 +191,7 @@ func eat(food):
 	for i in range(marknum/2, marknum):
 		markers_set[i].marktype = 1
 
-func manage_movement_and_collision(res):
+func manage_movement_and_collision(res, delta):
 	# if the ant collides stop moving
 		if res:
 			if res.collider.is_in_group("Food"):
@@ -162,15 +199,20 @@ func manage_movement_and_collision(res):
 				
 			# collide off the wall
 			if res.collider.is_in_group("hedge"):
-				desired_direction = pow(-1, randi()%2)*Vector2(desired_direction.y, -desired_direction.x)
+				
+				desired_direction = (desired_direction.bounce(res.normal)).normalized()
 				if markers_set:
 					markers_set[-1].destroy_marker()
 				
 			if res.collider.is_in_group("queen"):
 				reached_home()
+				
+			if res.collider.is_in_group("ant"):
+				fight(res.collider, delta)
 
 
 func reached_home():
+	food_carried = 0
 	get_parent().player_food += food_carried
 	get_parent().get_node("UI").display_food(get_parent().player_food)
 	hunger = hunger_max
--- scripts/game.gd
+++ scripts/game.gd
@@ -6,40 +6,64 @@ var lettuce : Resource = preload("res://scenes/lettuce.tscn")
 onready var cam : Camera2D = $Camera2D
 
 export var cam_speed = 300
+export var starting_ants = 50
 
 onready var ui = $UI
 
 onready var lett_spawner = $LettuceSpawner
 
-var ant_home : Vector2 = Vector2(600,500)
+var ant_home : Vector2 = Vector2(80,80)
 var markers : Array = []
 var player_food : int = 10
 
 func _ready():
-	cam.limit_right = 2040
-	cam.limit_bottom = 1150
+	cam.limit_right = 2048
+	cam.limit_bottom = 1152
 	cam.limit_top = 0 
 	cam.limit_left = 0
 
 	ui.connect("birth_ant", self, "on_birth_ant")
 	
-	var q = queen.instance()
-	add_child(q)
-	q.translate(ant_home)
-	q.modulate = Color(1,0.1,0.2)
+	var playerqueen = queen.instance()
+	add_child(playerqueen)
+	playerqueen.translate(ant_home)
+	playerqueen.modulate = Color(1,0.1,0.2)
 	
 	randomize()
-	for i in range(30):
+	for i in range(starting_ants):
 		var ant1 = ant.instance()
 		add_child(ant1)
 		ant1.modulate = Color(0.7,0,0.2)
 		ant1.scale /= 2
-		ant1.translate( ant_home)
-		ant1.set_ant_home(q)
+		ant1.translate( ant_home + Vector2(20,20))
+		ant1.set_ant_home(playerqueen)
+		ant1.set_collision_layer_bit(4, true)
+		ant1.set_collision_mask_bit(3 , true)
+		
+	ant_home = Vector2(1975, 1080)
+	
+	var enemyqueen = queen.instance()
+	add_child(enemyqueen)
+	enemyqueen.translate(ant_home)
+	enemyqueen.modulate = Color(0.3,0.05,0.1)
+	
+	for i in range(starting_ants):
+		var ant1 = ant.instance()
+		add_child(ant1)
+		ant1.modulate = Color(0.25,0.05,0.1)
+		ant1.scale /= 2
+		ant1.translate( ant_home - Vector2(20,20))
+		ant1.set_ant_home(enemyqueen)
+		ant1.set_collision_layer_bit(3, true)
+		ant1.set_collision_mask_bit(4 , true)
+		
+		
+	playerqueen.look_at(enemyqueen.position)	
+	enemyqueen.look_at(playerqueen.position)
 	
 		
 	lett_spawner.setup(0,2048,0,1152,10,50,10)
-	for n in 5:
+	for n in 25:
 		lett_spawner.spawn_bunch()
 
 func _process(delta):
